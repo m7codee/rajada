0f6d739fefafd3f95dda2976f1e417e55aea4d32
m7codee <104729995+m7codee@users.noreply.github.com>
2022-10-23 15:53:47 -0300

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,10 @@
+apt update;
+apt upgrade -y;
+apt install git -y;
+termux-setup-storage;
+cd storage/downloads;
+git clone https://github.com/m7codee/rajada;
+cd rajada;
+apt install python3 -y
+echo "Metodo de rajada - 10/s";
+python3 rajada.py;
