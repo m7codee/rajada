28af1df2c845b78744a0dfc46ab9742f7acb07b3
m7codee <104729995+m7codee@users.noreply.github.com>
2022-10-24 01:20:43 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,9 +8,6 @@ git clone https://github.com/m7codee/rajada;
 cd rajada;
 apt install python3 -y
 echo "Metodo de rajada - 10/s";
-pip3 install pyaes;
-pip3 install pyautogui;
-pip3 install pillow;
 while :
 do
 python3 rajada.py;
